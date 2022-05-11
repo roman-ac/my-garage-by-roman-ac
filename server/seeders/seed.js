1c8e00513791d80e2ad139fddef19f723c0a148a
Roman Ac <roman25xx@gmail.com>
2022-05-11 14:05:18 +1000

    seed.js added services to each car

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -43,9 +43,9 @@ db.once('open', async () => {
     await Service.deleteMany({});
 
     // bulk create each model
-  const users = await User.insertMany(userSeeds);
-  const cars = await Car.insertMany(carSeeds);
-  const services = await Service.insertMany(serviceSeeds);
+  const users = await User.insertMany(userSeeds); // school
+  const cars = await Car.insertMany(carSeeds); // class
+  const services = await Service.insertMany(serviceSeeds); // professor
 
     
   for (newCar of cars) {
@@ -53,6 +53,11 @@ db.once('open', async () => {
     const tempUser = users[Math.floor(Math.random() * users.length)];
     tempUser.cars.push(newCar._id);
     await tempUser.save();
+
+    // randomly add services to each car
+    const tempService = services[Math.floor(Math.random() * services.length)];
+    newCar.Service = tempService._id;
+    await newCar.save();
 
   
   }

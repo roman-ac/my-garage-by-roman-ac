77e38a4b98a00ff69465d5dd022b1741419c6eb0
Roman Ac <roman25xx@gmail.com>
2022-05-12 20:18:43 +1000

    displaying single car page wip

--- client/src/pages/SingleCar.js
+++ client/src/pages/SingleCar.js
@@ -4,21 +4,21 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 
-import CommentList from '../components/CommentList';
-import CommentForm from '../components/CommentForm';
+import ServiceList from '../components/ServiceList';
+import ServiceForm from '../components/ServiceForm';
 
 import { QUERY_SINGLE_CAR } from '../utils/queries';
 
 const SingleCar = () => {
   // Use `useParams()` to retrieve value of the route parameter `:profileId`
-  const { thoughtId } = useParams();
+  const { carId } = useParams();
 
   const { loading, data } = useQuery(QUERY_SINGLE_CAR, {
     // pass URL parameter
     variables: { carId: carId },
   });
 
-  const thought = data?.thought || {};
+  const car = data?.car || {};
 
   if (loading) {
     return <div>Loading...</div>;
@@ -26,9 +26,9 @@ const SingleCar = () => {
   return (
     <div className="my-3">
       <h3 className="card-header bg-dark text-light p-2 m-0">
-        {thought.thoughtAuthor} <br />
+        {car.username} <br />
         <span style={{ fontSize: '1rem' }}>
-          had this thought on {thought.createdAt}
+          had this thought on {car.createdAt}
         </span>
       </h3>
       <div className="bg-light py-4">
@@ -41,15 +41,15 @@ const SingleCar = () => {
             lineHeight: '1.5',
           }}
         >
-          {thought.thoughtText}
+          {car.description}
         </blockquote>
       </div>
 
       <div className="my-5">
-        <CommentList comments={thought.comments} />
+        <ServiceList comments={car.make} />
       </div>
       <div className="m-3 p-4" style={{ border: '1px dotted #1a1a1a' }}>
-        <CommentForm thoughtId={thought._id} />
+        <ServiceForm carId={car._id} />
       </div>
     </div>
   );
